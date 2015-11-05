9a704ef37d5db394a01f9b6d4e40cbe5849e6ae6
inzamam malik <malikasinger@gmail.com>
2015-11-05 19:49:58 +0500

    play video completed

--- www/views/videos/videos.js
+++ www/views/videos/videos.js
@@ -1,7 +1,43 @@
 angular.module("starter")
 
-  .controller("videosController",function($scope){
+  .controller("videosController", function ($scope, $ionicModal) {
 
 
+    $scope.allImages = [{
+      'src': 'img/pic1.jpg'
+    }, {
+      'src': 'img/pic2.jpg'
+    }, {
+      'src': 'img/pic3.jpg'
+    }];
 
+    $scope.clipSrc = 'img/test.MP4';
+
+    $scope.showImages = function (index) {
+      $scope.activeSlide = index;
+      $scope.showModal('templates/image-popover.html');
+    };
+
+    $scope.playVideo = function (link) {
+      $scope.showModal(link);
+    };
+
+    $scope.showModal = function (templateUrl) {
+      $ionicModal.fromTemplateUrl(templateUrl, {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function (modal) {
+        $scope.modal = modal;
+        $scope.modal.show();
+      });
+    };
+
+    // Close the modal
+    $scope.closeModal = function () {
+      $scope.modal.hide();
+      $scope.modal.remove()
+    };
   });
+
+
+
